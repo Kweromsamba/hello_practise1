21413d4ac9202928d68944d5e9a9dee9f56c64de
stellah <msambastellah188@gmail.com>
2024-06-06 10:37:55 +0300

    my first simple python code for practising

---
+++ hello.py
@@ -0,0 +1,9 @@
+def factorial(n):
+if n == 0;
+return1;
+   else 
+   	return  n*factorial(n-1)
+   	numbers = 5
+   	result = factorial(number)
+   	print(f "The factorial of {number} is {result}")
+
